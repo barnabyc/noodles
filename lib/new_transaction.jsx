e937d9c38a5f711c0d965919de9cef4d4c6fe285
Barnaby Claydon <barnaby@swipely.com>
2014-05-21 23:25:42 -0400

    Update state handling for new transaction

--- lib/new_transaction.jsx
+++ lib/new_transaction.jsx
@@ -3,28 +3,46 @@ const NewTransaction = React.createClass({
     accounts: React.PropTypes.array
   },
 
+  getInitialState: function () {
+    return { };
+  },
+
   render: function () {
     const { accounts } = this.props;
+    const {
+      date,
+      credit,
+      debit,
+      name,
+      accountNumber,
+      notes
+    } = this.state;
 
     return (
       <tr>
-        <td><input onChange={_.partial(this.handleChange,'date')} type="text" placeholder="date" /></td>
-        <td><input onChange={_.partial(this.handleChange,'credit')} type="text" placeholder="credit" /></td>
-        <td><input onChange={_.partial(this.handleChange,'debit')} type="text" placeholder="debit" /></td>
-        <td><input onChange={_.partial(this.handleChange,'name')} type="text" placeholder="name" /></td>
+        <td><input value={date} onChange={_.partial(this.handleChange,'date')} type="text" placeholder="date" /></td>
+        <td><input value={credit} onChange={_.partial(this.handleChange,'credit')} type="text" placeholder="credit" /></td>
+        <td><input value={debit} onChange={_.partial(this.handleChange,'debit')} type="text" placeholder="debit" /></td>
+        <td><input value={name} onChange={_.partial(this.handleChange,'name')} type="text" placeholder="name" /></td>
 
         {accounts && accounts.map((account) => {
-          return <td><input onChange={_.partial(this.handleChange,'account')} type="radio" name="account" /></td>
+          const checked = account.accountNumber === accountNumber;
+
+          return <td><input checked={checked} value={account.accountNumber} onChange={_.partial(this.handleChange,'accountNumber')} type="radio" name="account" /></td>
         })}
 
         <td>-</td>
-        <td><input onChange={_.partial(this.handleChange,'notes')} type="text" placeholder="notes" /></td>
+        <td><input value={notes} onChange={_.partial(this.handleChange,'notes')} type="text" placeholder="notes" /></td>
       </tr>
     );
   },
 
-  handleChange: function (fieldName) {
-    debugger;
+  handleChange: function (fieldName, ev) {
+    let state = {};
+
+    state[ fieldName ] = ev.target.value;
+
+    this.setState( state );
   }
 });
 
